0326c872068782b9fbfc121a29000655d0a8a816
Matt Carlson <matthewthomascarlson@gmail.com>
2017-08-07 00:13:41 -0700

    improve responsiveness for search options

--- app/assets/javascripts/organization.js
+++ app/assets/javascripts/organization.js
@@ -1,19 +1,32 @@
 $(document).ready(function() {
-  if($(window).width() < 768) {
+  if ($(window).width() < 768) {
     $('#resizing_list').addClass('list-inline');
     $('#resizing_list').removeClass('list-group');
-  }else{
+  } else {
     $('#resizing_list').addClass('list-group');
     $('#resizing_list').removeClass('list-inline');
   }
 
+  if ($(window).width() < 420 || $(window).width() > 768 && $(window).width() < 1200 ) {
+    $('#accepts_any').text("Matches any filter");
+    $('#accepts_all').text("Matches all filters");
+  }
+
   $(window).on('resize', function() {
-    if($(window).width() < 768) {
+    if ($(window).width() < 768) {
       $('#resizing_list').addClass('list-inline');
       $('#resizing_list').removeClass('list-group');
-    }else{
+    } else {
       $('#resizing_list').addClass('list-group');
       $('#resizing_list').removeClass('list-inline');
+    }
+
+    if ($(window).width() < 420 || $(window).width() > 768 && $(window).width() < 1200 ) {
+      $('#accepts_any').text("Matches any filter");
+      $('#accepts_all').text("Matches all filters");
+    } else {
+      $('#accepts_any').text("Find organizations that accept any of these");
+      $('#accepts_all').text("Find organizations that accept all of these");
     }
   });
 });
